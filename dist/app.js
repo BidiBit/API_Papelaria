597fba25bd702f725f626cd4b425c0a041b43089
BidiBit <mvfs.0426@gmail.com>
2026-05-15 18:49:03 -0300

    Second

--- dist/app.js
+++ dist/app.js
@@ -9,14 +9,11 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT ?? 3000;
 app.use(express_1.default.json());
 const produtos = [];
-function hellworld(req, res) {
-    res.status(200).send('Hello World!!');
-}
 function filtraProdutoPorID(req, res) {
     try {
-        let id = req.params.id;
-        const produto = produtos.find((p) => p.id === id);
-        res.status(200).json({ ID: id });
+        let id = Number(req.params.id);
+        const produto = produtos.find(p => p.id === id);
+        res.status(200).json(produto);
     }
     catch (e) {
         res.status(404).json({ Message: "Produto não encontrado" });
@@ -33,18 +30,62 @@ function listarProdutos(req, res) {
 function novoProduto(req, res) {
     try {
         let data = req.body;
-        if (!data.nome || !data.preco || !data.fabricante) {
-            throw new Error("Favor enviar os valores corretos");
+        if (!data.id || !data.nome || !data.preco || !data.fabricante) {
+            res.status(400).json("Para criar um produto novo é necessário informar o id, nome, preço e o fabricante");
+            return;
         }
-        let produto = new Produto_1.Produto(1, data.nome, data.preco, data.fabricante);
+        let produto = new Produto_1.Produto(data.id, data.nome, data.preco, data.fabricante);
         produtos.push(produto);
         res.status(201).json(produto);
     }
     catch (e) {
-        res.status(400).json({ Message: "Necessário informar as informações do produto." });
+        res.status(400).json({ Message: "Dados inválidos enviados pelo usuário" });
     }
 }
-app.get('/API_PAPELARIA/Produto/:id', filtraProdutoPorID);
-app.get('/API_PAPELARIA/Produto', listarProdutos);
-app.post('/API_PAPELARIA/Produto', novoProduto);
+function atualizarProduto(req, res) {
+    try {
+        let id = Number(req.params.id);
+        const produto = produtos.find(p => p.id === id);
+        let newData = req.body;
+        if (!produto) {
+            res.status(404).json({ Message: "Produto não encontrado" });
+            return;
+        }
+        if (newData.nome) {
+            produto.nome = newData.nome;
+        }
+        if (newData.preco) {
+            produto.preco = newData.preco;
+        }
+        if (newData.fabricante) {
+            produto.fabricante.nome = newData.fabricante.nome;
+            produto.fabricante.endereco.cidade = newData.fabricante.endereco.cidade;
+            produto.fabricante.endereco.pais = newData.fabricante.endereco.pais;
+        }
+        res.status(200).json({ Message: "Produto Atualizado" });
+    }
+    catch (e) {
+        res.status(400).json({ Message: "Dados inválidos enviados pelo usuário" });
+    }
+}
+function removerProdutoPorID(req, res) {
+    try {
+        let id = Number(req.params.id);
+        const produto = produtos.findIndex(p => p.id === id);
+        if (produto === -1) {
+            res.status(404).json({ Message: "Produto não encontrado" });
+            return;
+        }
+        produtos.splice(produto, 1);
+        res.status(200).json({ Message: "Produto Removido" });
+    }
+    catch (e) {
+        res.status(400).json({ Message: "Dados inválidos enviados pelo usuário" });
+    }
+}
+app.get('/Api_Papelaria/Produto/:id', filtraProdutoPorID);
+app.get('/Api_Papelaria/Produto', listarProdutos);
+app.post('/Api_Papelaria/Produto', novoProduto);
+app.put('/Api_Papelaria/Produto/:id', atualizarProduto);
+app.delete('/Api_Papelaria/Produto/:id', removerProdutoPorID);
 app.listen(PORT, () => console.log(`API rodando na URL : http://localhost:${PORT}`));
--- src/app.ts
+++ src/app.ts
@@ -3,53 +3,116 @@ import { Produto } from "./Produto";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
 app.use(express.json());
 
-const produtos: Produto[] =[];
+const produtos: Produto[] = [];
 
-function hellworld(req: Request, res: Response):void{
-    res.status(200).send('Hello World!!');
-}
+function filtraProdutoPorID(req: Request, res: Response): void {
+    try {
+        let id: any = Number(req.params.id);
+        const produto: any = produtos.find(p => p.id === id);
 
-function filtraProdutoPorID(req: Request, res: Response):void{
-    try{
-        let id:any = req.params.id;
-        const produto = produtos.find((p) => p.id === id);
-        
-        res.status(200).json({ID: id});
+        if (!produto) {
+            res.status(404).json({ Message: "Produto não encontrado" });
+            return;
+        }
+    
+        res.status(200).json(produto);
 
-    }catch(e: unknown){
-        res.status(404).json({Message: "Produto não encontrado"});
+    } catch(e: unknown) {
+        res.status(400).json({ Message: "Dados inválidos enviados pelo usuário" });
     }
 }
 
-function listarProdutos(req: Request, res: Response):void{
-    try{
-        res.status(200).json(produtos);
-
-    }catch(e: unknown){
-        res.status(404).json({Message: "Não há nenhum produto cadastrado"});
-    }
-}
-
-function novoProduto(req: Request, res: Response):void{
-    try{
-        let data:any = req.body;
-        if(!data.nome || !data.preco || !data.fabricante){
-            throw new Error("Favor enviar os valores corretos");
+function listarProdutos(req: Request, res: Response): void {
+    try {
+        if(!produtos){
+            res.status(404).json({Message: "Não há nenhum produto cadastrado"});
+            return;
         }
 
-        let produto = new Produto(1,data.nome,data.preco,data.fabricante);
-        produtos.push(produto);
-        res.status(201).json(produto);
+        res.status(200).json(produtos);
 
-    }catch(e: unknown){
-        res.status(400).json({Message: "Necessário informar as informações do produto."});
+    } catch(e: unknown) {
+        res.status(500).json({ Message: "Erro interno da aplicação" });
     }
 }
 
-app.get('/API_PAPELARIA/Produto/:id', filtraProdutoPorID);
-app.get('/API_PAPELARIA/Produto', listarProdutos);
-app.post('/API_PAPELARIA/Produto', novoProduto);
+function novoProduto(req: Request, res: Response): void {
+    try {
+        let data: any = req.body;
+        
+        if (!data.id || !data.nome || !data.preco || !data.fabricante) {
+            res.status(400).json("Para criar um produto novo é necessário informar o id, nome, preço e o fabricante");
+            return;
+        }
+
+        let produto = new Produto(data.id, data.nome, data.preco, data.fabricante);
+        produtos.push(produto);
+        
+        res.status(201).json(produto);
+
+    } catch(e: unknown) {
+        res.status(400).json({ Message: "Dados inválidos enviados pelo usuário" });
+    }
+}
+
+function atualizarProduto(req: Request, res: Response): void {
+    try {
+        let id: any = Number(req.params.id);
+        const produto: any = produtos.find(p => p.id === id);
+        let newData: any = req.body;
+        
+        if (!produto) {
+            res.status(404).json({ Message: "Produto não encontrado" });
+            return;
+        }
+
+        if (newData.nome) {
+            produto.nome = newData.nome;
+        }
+        
+        if (newData.preco) {
+            produto.preco = newData.preco;
+        }
+        
+        if (newData.fabricante) {
+            produto.fabricante.nome = newData.fabricante.nome;
+            produto.fabricante.endereco.cidade = newData.fabricante.endereco.cidade;
+            produto.fabricante.endereco.pais = newData.fabricante.endereco.pais;
+        }
+        
+        res.status(200).json({ Message: "Produto Atualizado" });
+
+    } catch(e: unknown) {
+        res.status(400).json({ Message: "Dados inválidos enviados pelo usuário" });
+    }
+}
+
+function removerProdutoPorID(req: Request, res: Response): void {
+    try {
+        let id: any = Number(req.params.id);
+        const produto: any = produtos.findIndex(p => p.id === id);
+
+        if (produto === -1) {
+            res.status(404).json({ Message: "Produto não encontrado" });
+            return;
+        }
+        
+        produtos.splice(produto, 1);
+
+        res.status(200).json({ Message: "Produto Removido" });
+
+    } catch(e: unknown) {
+        res.status(400).json({ Message: "Dados inválidos enviados pelo usuário" });
+    }
+}
+
+app.get('/Api_Papelaria/Produto/:id', filtraProdutoPorID);
+app.get('/Api_Papelaria/Produto', listarProdutos);
+app.post('/Api_Papelaria/Produto', novoProduto);
+app.put('/Api_Papelaria/Produto/:id', atualizarProduto);
+app.delete('/Api_Papelaria/Produto/:id', removerProdutoPorID);
 
 app.listen(PORT, () => console.log(`API rodando na URL : http://localhost:${PORT}`));
